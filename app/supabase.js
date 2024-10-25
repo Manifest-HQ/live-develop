cd47955f2db6228c45d4d2a13ddce10b7bae24ea
Kevin Diaz <kevin@manifest-hq.com>
2024-10-26 04:10:52 +0900

    fix supabase key usage

--- app/supabase.js
+++ app/supabase.js
@@ -4,6 +4,6 @@ const supabaseUrl = 'https://uzskfrmzzqmvsaxzeimy.supabase.co'
 const supabasePublicKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6c2tmcm16enFtdnNheHplaW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2OTcwMTgsImV4cCI6MjA0NTI3MzAxOH0.oZfMNSvFdPDxtR62pusD9emmtNCrRLHdqAZhniK7kHc'
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabasePublicKey)
 
 export default supabase
