da329314f624a67219178a2ff21bede7cbbbddf7
Eduardo López <loama18@gmail.com>
2024-10-26 12:22:45 +0200

    fix supabase key

--- supabase.js
+++ supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // SUPABASE instance of the project
-const supabaseUrl = 'https://uzskfrmzzqmvsaxzeimy.supabase.co'
-const supabasePublicKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6c2tmcm16enFtdnNheHplaW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2OTcwMTgsImV4cCI6MjA0NTI3MzAxOH0.oZfMNSvFdPDxtR62pusD9emmtNCrRLHdqAZhniK7kHc'
+const supabaseUrl = '{{SUPABASE_URL}}'
+const supabasePublicKey = '{{SUPABASE_PUBLIC_KEY}}'
 
 const supabase = createClient(supabaseUrl, supabasePublicKey)
 
